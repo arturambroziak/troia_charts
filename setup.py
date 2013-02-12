ff08d3d21a435df158001d86137fcb6f42123f74
borysiam <borysiam@gmail.com>
2013-02-12 15:36:15 +0100

    fixed setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name=name,
     version=version,
     description='Troia Charts Generator',
-    install_requires=('troia_client',),
+    #install_requires=('troia_client',),
     dependency_links=('https://github.com/kkonrad/troia-python-client.git#egg=troia_client',),
     scripts=('eval_script.py',)
 )
